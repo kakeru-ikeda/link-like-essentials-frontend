380ac3a18408fc65a41d04b1b9ea5b1fbe3a63a5
kakeru-ikeda <kakerumail1451@gmail.com>
2025-12-13 10:15:57 +0900

    [LLES-56] DeckDashboardコンポーネントを追加し、Homeコンポーネントに統合

---
+++ components/deck/DeckDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const DeckDashboard: React.FC = () => {
+  return (
+    <div className="flex-1 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center">
+      <p className="text-gray-400 text-sm">今後使用予定</p>
+    </div>
+  );
+};
