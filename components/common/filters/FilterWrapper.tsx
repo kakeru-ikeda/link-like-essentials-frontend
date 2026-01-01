b022b055808d200e380b5174f486bd5017f35322
ryo hasegawa <minecraftnolisio@gmail.com>
2026-01-01 11:17:59 +0900

    [LLES-70] FilterWrapperコンポーネントを新規作成し、子要素をラップする機能を追加

---
+++ components/common/filters/FilterWrapper.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface FilterWrapperProps {
+  children: React.ReactNode;
+}
+
+export const FilterWrapper: React.FC<FilterWrapperProps> = ({ children }) => {
+  return (
+    <div className="border border-gray-200 rounded-lg bg-gray-50">
+      {children}
+    </div>
+  );
+};
+
+export default FilterWrapper;
